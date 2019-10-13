e4246e879d5bb4dbb644734bb26aaf1b9d1246bb
Ekaterina <bazavei94@ gmail.com>
2019-10-13 12:13:29 +0700

    Настраивает работу с блоком backend

--- js/setup.js
+++ js/setup.js
@@ -2,13 +2,14 @@
 
 (function () {
   var setup = document.querySelector('.setup');
+  var form = setup.querySelector('.setup-wizard-form');
   var coatColor = setup.querySelector('.wizard-coat');
   var hiddenCoatInput = setup.querySelector('input[name="coat-color"]');
   var eyesColor = setup.querySelector('.wizard-eyes');
   var hiddenEyesInput = setup.querySelector('input[name="eyes-color"]');
   var fireballColor = setup.querySelector('.setup-fireball-wrap');
   var hiddenFireballInput = setup.querySelector('input[name="fireball-color"]');
-  var wizards = window.data.getWizardsArr;
+  // var wizards = window.data.getWizardsArr;
 
 
   var changeCoatColor = function () {
@@ -54,16 +55,50 @@
     return wizardElement;
   };
 
-  var renderWizards = function (array) {
+  // var renderWizards = function (array) {
+  //   var fragment = document.createDocumentFragment();
+  //   for (var j = 0; j < array.length; j++) {
+  //     fragment.appendChild(renderWizard(array[j]));
+  //   }
+  //   similarListElement.appendChild(fragment);
+  // };
+  // renderWizards(wizards);
+
+  // setup.querySelector('.setup-similar').classList.remove('hidden');
+
+  var onLoad = function (wizards) {
     var fragment = document.createDocumentFragment();
-    for (var j = 0; j < array.length; j++) {
-      fragment.appendChild(renderWizard(array[j]));
+
+    for (var i = 0; i < 4; i++) {
+      fragment.appendChild(renderWizard(wizards[i]));
     }
     similarListElement.appendChild(fragment);
-  };
-  renderWizards(wizards);
 
-  setup.querySelector('.setup-similar').classList.remove('hidden');
+    document.querySelector('.setup-similar').classList.remove('hidden');
+  };
+
+  var onError = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  var onSave = function () {
+    setup.classList.add('hidden');
+  };
+
+  form.addEventListener('submit', function (evt) {
+    window.backend.save(new FormData(form), onSave, onError);
+    evt.preventDefault();
+  });
+
+  window.backend.load(onLoad, onError);
 })();
 
 
